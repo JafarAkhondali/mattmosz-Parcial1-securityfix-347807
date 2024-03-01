a29626eb5384de0c0e65cb348089da0d5d945311
mattmosz <matias.mosquera619@gmail.com>
2024-03-01 12:18:33 -0500

    feat: add ajax estudiantes

---
+++ administrador/models/estudiantes/ajax-estudiantes.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '../../../includes/conexion.php';
+if(!empty($_POST)){
+    if(empty($_POST['NOMBRE']) || empty($_POST['EDAD']) || empty($_POST['CARRERA'])){
+        $respuesta = array('status' => false,'msg' => 'Todos los campos son necesarios');
+    }else{
+        $nombre = $_POST['nombreE'];
+        $edad = $_POST['edad'];
+        $carrera = $_POST['carrera'];
+        $promedio = $_POST['promedio'];
+
+        $sql = 'SELECT * FROM estudiante WHERE usuario = ?';
+        $query = $pdo->prepare($sql);
+        $query->execute(array($nombre));
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        if($result > 0){
+            $respuesta = array('status' => false, 'msg' => 'El usuario ya existe');
+        }else{
+            $sqlInsert = 'INSERT INTO estudiante (nombre, edad, carrera, promedio) VALUES (?, ?, ?, ?)';
+            $queryInsert = $pdo->prepare($sqlInsert);
+            $resultInsert = $queryInsert->execute(array($nombre, $edad, $carrera, $promedio));
+            if($resultInsert){
+                $respuesta = array('status' => true, 'msg' => 'Estudiante registrado correctamente');
+            }else{
+                $respuesta = array('status' => false, 'msg' => 'Error al registrar el estudiante');
+            }
+        }
+    }
+    echo json_encode($respuesta, JSON_UNESCAPED_UNICODE);
+}
+
+?>
